8229c8cb14362897fd1078d6006cbf0cb1a453e0
Aspecky <tarrihmoad@proton.me>
2025-09-26 18:52:41 +0100

    Add slnode_free

--- source/SinglyLinkedNode/free.c
+++ source/SinglyLinkedNode/free.c
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   SinglyLinkedNode.h                                 :+:      :+:    :+:   */
+/*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/05 14:29:20 by mtarrih           #+#    #+#             */
-/*   Updated: 2025/09/26 18:52:14 by mtarrih          ###   ########.fr       */
+/*   Created: 2025/09/26 18:48:38 by mtarrih           #+#    #+#             */
+/*   Updated: 2025/09/26 18:52:07 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SINGLYLINKEDNODE_H
-# define SINGLYLINKEDNODE_H
+#include "SinglyLinkedNode.h"
+#include <stdlib.h>
 
-typedef struct s_slnode
+void	slnode_free(t_slnode *node, void (*del)(void *))
 {
-	struct s_slnode		*next;
-	void				*data;
-}	t_slnode;
-
-t_slnode	*slnode_new(void *data);
-t_slnode	*slnode_last(t_slnode *head);
-t_slnode	*slnode_previous(t_slnode *head, t_slnode *node);
-void		slnode_free(t_slnode *node, void (*del)(void *));
-
-#endif
+	del(node->data);
+	free(node);
+}
