2aecb23f77f0b807f7f6dc7dcf3b4707d9aae6fa
Aspecky <tarrihmoad@proton.me>
2025-09-27 18:27:47 +0100

    Removed redundant check in `sllist_clear`

--- source/SinglyLinkedList/clear.c
+++ source/SinglyLinkedList/clear.c
@@ -6,7 +6,7 @@
 /*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 22:00:39 by mtarrih           #+#    #+#             */
-/*   Updated: 2025/03/31 06:03:57 by mtarrih          ###   ########.fr       */
+/*   Updated: 2025/09/27 18:26:44 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,13 @@ void	sllist_clear(t_sllist *list, void (*del)(void *))
 	t_slnode	*next;
 
 	head = list->head;
-	if (head)
+	while (head)
 	{
-		while (head)
-		{
-			next = head->next;
-			if (del)
-				del(head->data);
-			free(head);
-			head = next;
-		}
+		next = head->next;
+		if (del)
+			del(head->data);
+		free(head);
+		head = next;
 	}
 	list->head = 0;
 	list->tail = 0;
